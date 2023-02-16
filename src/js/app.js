6ca58aead9b7fbe2746fadacf0a8ee48d324497a
RonaldoDairo <programaciondairo@gmail.com>
2023-02-16 22:39:55 +0000

    Entiendo mas o menos

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,12 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name}</h1>
+          <h2>${variables.lastname}</h2>
+          <h3>${variables.socialMediaPosition}</h3>
+          <h4>${variables.twitter}</h4>
+          <h5>${variables.linkedin}</h5>
+          
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
